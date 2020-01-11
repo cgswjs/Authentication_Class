6b69257db6b9e5314e7b544c4f1d7b417ce75f41
Ian <37604990+cgswjs@users.noreply.github.com>
2020-01-11 13:29:13 -0800

    Implemented Login With Google Through Oauth2.0

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const findOrCreate = require("mongoose-findorcreate");
 //cookie
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 //use bcrypt
 const bcrypt = require('bcrypt');
 const saltRound = 10;
@@ -20,40 +23,77 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //session has to be put right under all other app.use but
 //beyond mongoose.connect
+
 app.use(session({
   secret:"Our little secret.",
   resave:false,
   saveUninitialized:false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-//--------------------------------------------------//
-
+//-----------------SET UP MONGOOSE------------------------//
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true})
 mongoose.set("useCreateIndex",true);
 
+//Schema is a template of collection
 const userSchema = new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  //add this googleId for findOrCreate method
+  googleId:String
 });
 
 //use passport local mongoose
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
+//initialize a model of data
 const User = new mongoose.model("User",userSchema);
 
 //create a local login strategy
 passport.use(User.createStrategy());
-//create cookie
-passport.serializeUser(User.serializeUser());
-//destroy cookie
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//this has to put here
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 //-----------------RESTful API--------------------//
 app.get("/",function(req,res){
   res.render("home");
 });
+
+//this method allows using google to register or login
+app.get("/auth/google",
+  passport.authenticate("google",{scope:['profile']})
+);
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login",function(req,res){
   res.render("login");
@@ -110,21 +150,6 @@ app.post("/login",function(req,res){
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
